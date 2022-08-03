3ae178d3737916606d194fc2cf8c3df190d4fe4b
Benjamin Bohec <bbohec.pro@gmail.com>
2022-08-03 15:17:08 +0200

    done

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,25 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { tick } from './tick/tick'
-import  gsap from 'gsap'
 
+
+
+
+// CURSOR
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = event.clientY / sizes.height - 0.5;
+})
 
 
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas:HTMLElement|null = document.querySelector('canvas.webgl')
 if (!canvas) throw new Error('Canvas not found')
 
 
@@ -18,6 +30,7 @@ const scene = new THREE.Scene()
 const sizes = {
     width: 800,
     height: 600
+    //height: 800
 }
 // Object
 const mesh = new THREE.Mesh(
@@ -26,13 +39,28 @@ const mesh = new THREE.Mesh(
 )
 scene.add(mesh)
 
+const verticalFov = 75
+// be aware of z-fighting glitches
+const near = 0.1 // very close
+const far = 100 // quite far according to current meshs that are viewed
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(verticalFov, aspectRatio, near ,far)
+// const camera = new THREE.OrthographicCamera(
+//     -1*aspectRatio,
+//     1*aspectRatio,
+//     1,
+//     -1,
+//     near,
+//     far
+// )
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -41,13 +69,25 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 // Animate
-const clock = new THREE.Clock()
+//const clock = new THREE.Clock()
+
+const controls = new OrbitControls(camera, canvas)
+// controls.target.y = 2
+// controls.update()
+controls.enableDamping = true
 
 const onTick = () => {
-    const elapsedTime = clock.getElapsedTime()
+    //const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    mesh.rotation.y = elapsedTime;
+    // // Update objects
+    // mesh.rotation.y = elapsedTime;
+
+    // // Update camera without builtin controls
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * Math.PI
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * Math.PI
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
